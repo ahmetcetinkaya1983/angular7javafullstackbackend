7a01961963ba33224250e40709676d6e336e7047
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-04-14 05:56:00 +0300

    delete applied

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -5,23 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @CrossOrigin(origins="http://localhost:4200")
+@RestController
 public class HelloWorldController {
 	
 	
-	@GetMapping(path="/hello-world")
+	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "hello world";
 	}
 	
-	@GetMapping(path="/hello-world-bean")
+	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		//throw new RuntimeException("An error occured");
 		return new HelloWorldBean("hello world");
 	}
 	
-	@GetMapping(path="/hello-world-bean-variable/{name}")
+	@GetMapping("/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		
 		return new HelloWorldBean("hello world,  "+ name);
